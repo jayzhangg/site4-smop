e11f7b4e0d6a90232dbd60357d1aca525f138644
AlexShukhman <alex.jacob.shukhman@gmail.com>
2017-01-26 19:49:45 -0500

    doesn't work, but want to get mac up to date

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,12 @@ app.use(bodyParser.urlencoded({
 	extended: true
 }));
 var router = express.Router();
+const MongoClient = require('mongodb').MongoClient
+var db
+MongoClient.connect('mongodb://<alexshukhman>:<smop1>@ds145128.mlab.com:45128/smop', (err, database) => {
+	if (err) return console.log(err)
+	db = database
+});
 /* GET home page. */
 router.get('/', function (req, res) {
 	res.render('index');
@@ -17,6 +23,17 @@ router.get('/coder_home', (req, res) => {
 	res.render('coder_home');
 });
 router.post('/login', (req, res) => {
-	console.log(req.body);
+	db.collection('loginreqs').save(req.body, (err, result) => {
+		app.post('/quotes', (req, res) => {
+			db.collection('quotes').save(req.body, (err, result) => {
+				if (err) return console.log(err)
+				console.log('saved to database')
+				res.redirect('/')
+			})
+		})
+	});
+	if (err) return console.log(err);
+	console.log('saved to database');
+	res.redirect('/options');
 });
 module.exports = router;
