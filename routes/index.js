d4e2cbdb9cbd315c35ed78882c0e4e05320e6ab2
AlexShukhman <alex.jacob.shukhman@gmail.com>
2017-01-30 13:36:10 -0500

    did a bunch of api work

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,12 @@
 var express = require('express');
 var app = express();
+var http = require('http');
+var qs = require("querystring")
+var token = '';
+var apicall = {
+	host: 'localhost:3001/'
+	, port: 3001
+};
 const bodyParser = require('body-parser');
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({
@@ -18,6 +25,37 @@ router.get('/coder_home', (req, res) => {
 	res.render('coder_home');
 });
 router.post('/login', (req, res) => {
-	res.redirect('/options');
+	var options = {
+		"method": "POST"
+		, "hostname": "localhost"
+		, "port": "3001"
+		, "path": "/api/authenticate"
+		, "headers": {
+			"content-type": "application/x-www-form-urlencoded"
+			, "cache-control": "no-cache"
+			, "postman-token": "d6e25f06-55a2-0e52-dba9-f3c94373705b"
+		}
+	};
+	var req = http.request(options, function (result) {
+		var chunks = [];
+		result.on("data", function (chunk) {
+			chunks.push(chunk);
+		});
+		result.on("end", function () {
+			var body = JSON.parse(Buffer.concat(chunks).toString());
+			if (body.success) {
+				token = body.token;
+				res.redirect('/options');
+			}
+			else {
+				res.redirect('/');
+			}
+		});
+	});
+	req.write(qs.stringify({
+		name: 'alexshukhman'
+		, password: 'smop1'
+	}));
+	req.end();
 });
 module.exports = router;
