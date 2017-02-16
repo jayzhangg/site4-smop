425991edcb06ca5ac77c5e297b64f77b8ab5bc1b
AlexShukhman <alex.jacob.shukhman@gmail.com>
2017-02-16 15:31:33 -0500

    messed with ace and made everything work with no errors

--- public/javascripts/coder_home.js
+++ public/javascripts/coder_home.js
@@ -9,7 +9,5 @@ WHY ARE YOU DIVING SO DEEP INTO MY CODE??
 */
 // Input the data from the database to the ace.js editor
 (function () {
-	//var info = JSON.parse(JSON.stringify($("info")));
-	console.log(info);
 	$("#editor").html(info);
 })();
