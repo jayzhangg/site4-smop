87f3fae080b1b9a188432b330b8ee403ae1bf249
AlexShukhman <alex.jacob.shukhman@gmail.com>
2017-10-07 00:23:24 -0500

    fixing login issue

--- routes/index.js
+++ routes/index.js
@@ -156,7 +156,7 @@ router.get('/new_user', function (req, res) {
 });
 router.get('/options', (req, res) => {
 	// check if token expired
-	var ssn = req.headers['ssn'];
+	var ssn = req.get('ssn');
 	if (!ssn) {
 		console.log('redirection asshole');
 		res.redirect('/');
