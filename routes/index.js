d854acc7e7c768f1f114abedf721bf71a7f7aba7
AlexShukhman <alex.jacob.shukhman@gmail.com>
2017-10-24 23:59:51 -0400

    email should be working now

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,7 @@ var crypto = require('crypto');
 var hostname = '18.220.173.197';
 //var hostname = "localhost";
 var port = '';
-// var port = '3000'
+//var port = '3000'
 var genRandomString = function (length) {
 	return crypto.randomBytes(Math.ceil(length / 2)).toString('hex') /** convert to hexadecimal format */ .slice(0, length); /** return required number of characters */
 };
